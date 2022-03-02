f562a78140252fdeb33986a2223c87977b3f1a9b
Shivani (Contractor) Mangla <c-smangla@dovercorp.com>
2022-03-03 01:22:37 +0530

    Learnt about Scaffold Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
+import 'package:first_application/home_page.dart';
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-
- class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-      child: Center(
-      child: Text('test application'),
-      )
-    ));
+    return const MaterialApp(
+      home: Homepage(),
+    );
   }
 }
