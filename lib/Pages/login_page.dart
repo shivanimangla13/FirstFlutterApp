496d4edec2dc48e01f712f79617f704bf3de5946
Shivani (Contractor) Mangla <c-smangla@dovercorp.com>
2022-03-08 00:56:14 +0530

    learnt animation using AnimatedContainer

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:first_application/utils/routes.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "test";
+  bool isLoginButtonCLicked = false;
   @override
   Widget build(BuildContext context) {
     int a = 30;
@@ -23,29 +30,57 @@ class LoginPage extends StatelessWidget {
             height: 150,
           ),
           const SizedBox(height: 10),
+          Text(name),
           Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(children: [
                 TextFormField(
                   decoration: const InputDecoration(
-                      labelText: 'Email or phone number *',
+                      labelText: 'Email or phone number',
                       fillColor: Colors.grey,
                       filled: true),
                   cursorColor: Colors.white,
+                  onChanged: (data) {
+                    setState(() {
+                      name = "data";
+                    });
+                  },
                 ),
                 TextFormField(
                   decoration: const InputDecoration(labelText: 'Password *'),
                   cursorColor: Colors.black,
                 ),
               ])),
-          ElevatedButton(
-            onPressed: () => {Navigator.pushNamed(context, MyRoutes.homeRoute)},
-            child: const Text("Login"),
-            style: ButtonStyle(
-                fixedSize: MaterialStateProperty.all<Size>(const Size(320, 50)),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.red)),
-          )
+          InkWell(
+              onTap: () async {
+                setState(() {
+                  isLoginButtonCLicked = !isLoginButtonCLicked;
+                });
+                await Future.delayed(const Duration(seconds: 1));
+                Navigator.pushNamed(context, MyRoutes.homeRoute);
+              },
+              child: AnimatedContainer(
+                duration: const Duration(seconds: 1),
+                height: 50,
+                width: isLoginButtonCLicked ? 50 : 150,
+                child: Text(isLoginButtonCLicked ? "@" : "Login",
+                    style: const TextStyle(
+                        color: Colors.black, fontWeight: FontWeight.bold)),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius:
+                        BorderRadius.circular(isLoginButtonCLicked ? 25 : 8)),
+              ))
+
+          // ElevatedButton(
+          //   onPressed: () => {Navigator.pushNamed(context, MyRoutes.homeRoute)},
+          //   child: const Text("Login"),
+          //   style: ButtonStyle(
+          //       fixedSize: MaterialStateProperty.all<Size>(const Size(320, 50)),
+          //       foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          //       backgroundColor: MaterialStateProperty.all<Color>(Colors.red)),
+          // )
         ],
       )),
       backgroundColor: Colors.white,
