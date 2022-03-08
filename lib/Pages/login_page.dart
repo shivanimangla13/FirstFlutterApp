b5580a8406fbd07bb6e515c420445c6736645f67
Shivani (Contractor) Mangla <c-smangla@dovercorp.com>
2022-03-08 11:37:41 +0530

    ripple effect and input validation

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -9,80 +9,92 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  String name = "test";
   bool isLoginButtonCLicked = false;
+  final _key = GlobalKey<FormState>();
+  void onLogin() async {
+    if (_key.currentState!.validate()) {
+      setState(() {
+        isLoginButtonCLicked = true;
+      });
+      await Future.delayed(const Duration(seconds: 1));
+      Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        isLoginButtonCLicked = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     int a = 30;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("NETFLIX"),
-        backgroundColor: Colors.black,
+        title: const Text("WELCOME"),
+        backgroundColor: Colors.deepPurple,
         titleTextStyle: const TextStyle(
-            fontWeight: FontWeight.bold, color: Colors.red, fontSize: 50),
+            fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
       ),
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Image.asset(
-            "assets/images/login.png",
-            fit: BoxFit.cover,
-            height: 150,
-          ),
-          const SizedBox(height: 10),
-          Text(name),
-          Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: 'Email or phone number',
-                      fillColor: Colors.grey,
-                      filled: true),
-                  cursorColor: Colors.white,
-                  onChanged: (data) {
-                    setState(() {
-                      name = "data";
-                    });
-                  },
+        child: Form(
+            key: _key,
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/images/login.png",
+                  fit: BoxFit.cover,
+                  height: 150,
                 ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Password *'),
-                  cursorColor: Colors.black,
-                ),
-              ])),
-          InkWell(
-              onTap: () async {
-                setState(() {
-                  isLoginButtonCLicked = !isLoginButtonCLicked;
-                });
-                await Future.delayed(const Duration(seconds: 1));
-                Navigator.pushNamed(context, MyRoutes.homeRoute);
-              },
-              child: AnimatedContainer(
-                duration: const Duration(seconds: 1),
-                height: 50,
-                width: isLoginButtonCLicked ? 50 : 150,
-                child: Text(isLoginButtonCLicked ? "@" : "Login",
-                    style: const TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold)),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                    color: Colors.red,
+                const SizedBox(height: 10),
+                Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                            labelText: 'Email or phone number'),
+                        onChanged: (data) {},
+                        validator: (value) {
+                          if (value == "") {
+                            return "Email cannot be blank";
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        decoration:
+                            const InputDecoration(labelText: 'Password *'),
+                        cursorColor: Colors.black,
+                        validator: (value) {
+                          if (value == "") {
+                            return "Password cannot be blank";
+                          } else if (value!.length < 6) {
+                            return "Password should be greater than 6 characters";
+                          }
+                          return null;
+                        },
+                      ),
+                    ])),
+                Material(
+                    color: Colors.deepPurple,
                     borderRadius:
-                        BorderRadius.circular(isLoginButtonCLicked ? 25 : 8)),
-              ))
-
-          // ElevatedButton(
-          //   onPressed: () => {Navigator.pushNamed(context, MyRoutes.homeRoute)},
-          //   child: const Text("Login"),
-          //   style: ButtonStyle(
-          //       fixedSize: MaterialStateProperty.all<Size>(const Size(320, 50)),
-          //       foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-          //       backgroundColor: MaterialStateProperty.all<Color>(Colors.red)),
-          // )
-        ],
-      )),
+                        BorderRadius.circular(isLoginButtonCLicked ? 25 : 8),
+                    child: InkWell(
+                      onTap: () {
+                        onLogin();
+                      },
+                      child: AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          height: 50,
+                          width: isLoginButtonCLicked ? 50 : 150,
+                          child: Text(isLoginButtonCLicked ? "@" : "Login",
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                          alignment: Alignment.center),
+                    ))
+              ],
+            )),
+      ),
       backgroundColor: Colors.white,
     );
   }
