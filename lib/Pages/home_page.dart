85f6da9e45e270abd9b8d74d254a3c62f1f96f01
Shivani (Contractor) Mangla <c-smangla@dovercorp.com>
2022-03-09 16:44:05 +0530

    loaded local json data into listview

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'dart:convert';
 import 'dart:math';
 import 'dart:ui';
 
@@ -5,14 +8,35 @@ import 'package:first_application/models/catalogue.dart';
 import 'package:first_application/widgets/drawer.dart';
 import 'package:first_application/widgets/items_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   void ListItemTapped() {}
+  var list = <Catalogue>[];
+  @override
+  void initState() {
+    super.initState();
+    LoadData();
+  }
+
+  LoadData() async {
+    final json = await rootBundle.loadString("assets/files/catalogue.json");
+    final decodeData = jsonDecode(json);
+    var productsData = decodeData["products"];
+    list = List.from(productsData).map((e) => Catalogue.fromMap(e)).toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(5, (index) => Items.list[0]);
-    int a = 30;
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -24,21 +48,24 @@ class Homepage extends StatelessWidget {
             const TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
         foregroundColor: Colors.black,
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) => Card(
-          child: (ListTile(
-            leading: Image.asset("assets/images/iPhone.jpg"),
-            title: Text(dummyList[index].name),
-            trailing: Text(
-              "\$${dummyList[index].price.toString()}",
-              style: const TextStyle(color: Colors.deepPurple),
-              textScaleFactor: 1.2,
-            ),
-            onTap: ListItemTapped,
-          )),
-        ),
-        itemCount: dummyList.length,
-      ),
+      body: list != null && list.isNotEmpty
+          ? ListView.builder(
+              itemBuilder: (context, index) => Card(
+                child: (ListTile(
+                  leading: Image.network(list[index].image),
+                  title: Text(list[index].name),
+                  trailing: Text(
+                    "\$${list[index].price.toString()}",
+                    style: const TextStyle(
+                        color: Colors.deepPurple, fontWeight: FontWeight.bold),
+                    textScaleFactor: 1.2,
+                  ),
+                  onTap: ListItemTapped,
+                )),
+              ),
+              itemCount: list.length,
+            )
+          : const Center(child: CircularProgressIndicator()),
       drawer: const MyDrawer(),
     );
   }
--- lib/models/catalogue.dart
+++ lib/models/catalogue.dart
@@ -1,18 +1,24 @@
 class Catalogue {
-  final num id;
-  final String name;
-  final String description;
-  final num price;
-  final String image;
+  num id;
+  String name;
+  String description;
+  num price;
+  String image;
 
   Catalogue(this.id, this.name, this.description, this.price, this.image);
+  factory Catalogue.fromMap(Map<String, dynamic> map) {
+    var data = Catalogue(
+        map["id"], map["name"], map["desc"], map["price"], map["image"]);
+    return data;
+  }
 }
 
-class Items {
-  static List<Catalogue> list = [
-    Catalogue(1, "iPhone 12", "Apple product", 999, ""),
-    Catalogue(1, "iPhone 13", "Apple product", 999, ""),
-    Catalogue(1, "iPhone 12 Pro", "Apple product", 999, ""),
-    Catalogue(1, "iPhone 13 Pro", "Apple product", 999, ""),
-  ];
-}
+
+// class Items {
+//   static List<Catalogue> list = [
+//     Catalogue(1, "iPhone 12", "Apple product", 999, ""),
+//     Catalogue(1, "iPhone 13", "Apple product", 999, ""),
+//     Catalogue(1, "iPhone 12 Pro", "Apple product", 999, ""),
+//     Catalogue(1, "iPhone 13 Pro", "Apple product", 999, ""),
+//   ];
+//}
